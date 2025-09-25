a2987ab76f629271d5b4eb1cd9c83610c181b5c6
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2025-09-25 14:24:14 +0200

    feat: improve report (formatting, break detection)
    - Add rich library for better console output formatting
    - Implement automatic lunch break detection based on longest break between 10:00-15:00
    - Add verbose mode (-v, -vv) to show break details
    - Display breaks >20 minutes in detailed view
    - Improve date/time formatting in report output

--- worktime/report.py
+++ worktime/report.py
@@ -1,51 +1,100 @@
 #!/usr/bin/env python
 
-from datetime import datetime, timedelta
-from pathlib import Path
-
 import argparse
 import sys
+from datetime import datetime, time, timedelta
+from pathlib import Path
+
+from rich.console import Console
+from rich.table import Table, box
 
 FOLDER_FORMAT = "%Y-%m-%d"
 FILENAME_FORMAT = "%Y-%m-%d-%H-%M"
-REPORT_FORMAT = "{:10} {:10} {:10} {:10} {}"
 
 
-def report_day(args, date):
+def is_lunch(t1, t2):
+    lunch_earliest = t1.combine(t1.date(), time(10))
+    lunch_latest = t1.combine(t1.date(), time(15))
+
+    return t1 >= lunch_earliest and t2 <= lunch_latest
+
+
+def report_day(table, args, date):
     folder = "{}/{}".format(args.folder, date.strftime(FOLDER_FORMAT))
     path = Path(folder)
     screenshots = [p.stem for p in list(path.glob("*.png"))]
     timestamps = sorted([datetime.strptime(s, FILENAME_FORMAT) for s in screenshots])
 
+    # Compute longest delta between consecutive timestamps
+    longest_break = timedelta(minutes=0)
+    longest_break_start = None
+    longest_break_end = None
+    long_breaks = []
+    if len(timestamps) > 1:
+        deltas = [(timestamps[i] - timestamps[i-1], timestamps[i-1], timestamps[i]) for i in range(1, len(timestamps))]
+        longest_break, longest_break_start, longest_break_end = max(deltas, key=lambda x: x[0])
+        
+        # Collect all breaks longer than 20 minutes for -vv mode
+        if args.verbose >= 2:
+            long_breaks = [(delta, start, end) for delta, start, end in deltas if delta > timedelta(minutes=20)]
+
     if len(timestamps) > 0:
         arrive = timestamps[0]
         leave = timestamps[-1]
         total = leave - arrive
-        lunch = args.lunch if args.lunch < total else timedelta(minutes=0)
+        lunch = longest_break if is_lunch(longest_break_start, longest_break_end) else args.lunch
+        if lunch > total:
+            lunch = timedelta(0)
         total -= lunch
-        print(
-            REPORT_FORMAT.format(
-                date.strftime("%a"),
-                arrive.strftime("%H:%M"),
-                leave.strftime("%H:%M"),
-                str(lunch)[0:-3],
-                str(total)[0:-3],
+
+        row = []
+        row.append(date.strftime('%-d/%-m %a'))
+        row.append(arrive.strftime('%H:%M'))
+        row.append(leave.strftime('%H:%M'))
+        row.append((datetime.min + lunch).strftime('%H:%M'))
+        if args.verbose >= 1:
+            row.append("{}-{}".format(
+                longest_break_start.strftime('%H:%M'),
+                longest_break_end.strftime('%H:%M')
+                )
             )
-        )
+        if args.verbose >= 2:
+            if long_breaks:
+                breaks_str = ", ".join([
+                    "{}-{} ({})".format(
+                        start.strftime('%H:%M'),
+                        end.strftime('%H:%M'),
+                        (datetime.min + delta).strftime('%H:%M')
+                    )
+                    for delta, start, end in long_breaks
+                ])
+                row.append(breaks_str)
+            else:
+                row.append("-")
+        row.append((datetime.min + total).strftime('%H:%M'))
+
+        table.add_row(*row)
 
 
 def report_week(args):
     start = datetime.strptime("{}-{}-1".format(args.year, args.week), "%G-%V-%u")
 
-    print(
-        REPORT_FORMAT.format(
-            "Week " + str(args.week), "Arrive", "Leave", "Lunch", "Total"
-        )
-    )
+    table = Table(title=f"Week {args.week}", box=box.SIMPLE)
+    table.add_column("Day")
+    table.add_column("Arrive")
+    table.add_column("Leave")
+    table.add_column("Lunch")
+    if args.verbose >= 1:
+        table.add_column("Break")
+    if args.verbose >= 2:
+        table.add_column("Long Breaks (>20m)")
+    table.add_column("Total")
 
     for date in [start + timedelta(days=days) for days in range(7)]:
-        report_day(args, date)
+        report_day(table, args, date)
 
+    console = Console()
+    console.print(table)
 
 def main(args):
     # Default to previous week
@@ -54,6 +103,10 @@ def main(args):
     default_week = default_start.isocalendar()[1]
 
     parser = argparse.ArgumentParser(description="Worktime report")
+
+    parser.add_argument(
+        "--verbose", "-v", action="count", default=0
+    )
 
     parser.add_argument(
         "--folder", "-f", help="screenshot folder", default=Path.home() / ".screenshots"
